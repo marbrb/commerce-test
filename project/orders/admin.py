10916e99958d77bae27e644f9d21cb530c5e700f
Miguel Rodriguez <marbrb1@gmail.com>
2018-11-30 12:48:28 -0500

    added admin links to improve navigation

--- project/orders/admin.py
+++ project/orders/admin.py
@@ -3,6 +3,8 @@ import json
 from django.contrib import admin
 from django.contrib import messages
 from django.urls import path
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 
@@ -11,10 +13,24 @@ from .models import Payment
 from .models import Product
 from .utils import reverse_payment
 
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
+
+    def _payment(self, obj):
+        if obj.payment:
+            return mark_safe('<a href="{}">{}</a>'.format(
+                reverse('admin:orders_payment_change', args=[obj.payment.id]),
+                str(obj.payment),
+            ))
+
+        return '-'
+
+    _payment.short_description = 'Datos del pago'
+    _payment.allow_tags = True
+
 
     list_display = (
         'id',
@@ -35,10 +51,10 @@ class OrderAdmin(admin.ModelAdmin):
     )
 
     fields = (
+        '_payment',
         'user',
         'products',
         'total_value',
-        'payment',
     )
 
     readonly_fields = fields
@@ -48,6 +64,18 @@ class OrderAdmin(admin.ModelAdmin):
 class PaymentAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
+
+    def _order(self, obj):
+        if obj.order:
+            return mark_safe('<a href="{}">{}</a>'.format(
+                reverse('admin:orders_order_change', args=[obj.order.id]),
+                str(obj.order),
+            ))
+
+        return '-'
+
+    _order.short_description = 'Datos del pago'
+    _order.allow_tags = True
 
     def create_response_json(self, obj):
         if not obj.create_response:
@@ -67,6 +95,7 @@ class PaymentAdmin(admin.ModelAdmin):
     )
 
     fields = (
+        '_order',
         'created_at',
         'status',
         'status_updated_at',
